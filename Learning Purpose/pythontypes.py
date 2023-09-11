17082e20c41ad1c91642371fe5372c8fd054342b
Akash-Softw <akashofficial881@gmail.com>
2023-09-11 13:05:39 +0530

    HTML,CSS includes and changes made in Main.py Accordingly

---
+++ Learning Purpose/pythontypes.py
@@ -0,0 +1,7 @@
+def add(firstname,lastname):
+ firstname+" "+lastname
+ return firstname+" "+lastname
+fname = "Akash"
+lname = "Mukherjee"
+name = add(fname,lname)
+
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+templates = Jinja2Templates(directory="templates")
+
+
+conn = MongoClient("mongodb+srv://akashmukherjee2908:7cc675bc89c1d9998046ff2615371132@akashcluster.io8ogmc.mongodb.net/notes")
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+   docs = conn.notes.notes.find({})
+   print(docs)
+   return templates.TemplateResponse("index.html", {"request": request})
